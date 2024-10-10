e19ca81522b05e02211ffd7b65a4ab29d7c626a6
hammadhk <hammadhk090@gmail.com>
2024-10-10 14:17:15 +0500

    first commit

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "go" : "https://www.youtube.com/watch?v=c8sMSxCTtBI&t=30s" ,
+    "nasheed" : "https://www.youtube.com/watch?v=dziMp-JS_ps",
+    "naat" : "https://www.youtube.com/watch?v=QkesgxJeeQk&list=RDQkesgxJeeQk&start_radio=1&rv=c8sMSxCTtBI",
+    "peace" : "https://www.youtube.com/watch?v=KRHg6yZw3r0"
+}
